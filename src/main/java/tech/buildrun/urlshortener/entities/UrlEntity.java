188180de10e247621b09fcb46d3ba578283e6de0
rafaelreissant <santireis14@gmail.com>
2025-12-16 18:14:53 -0300

    creating repository

--- src/main/java/tech/buildrun/urlshortener/entities/UrlEntity.java
+++ src/main/java/tech/buildrun/urlshortener/entities/UrlEntity.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Document(collection = "urls")
-public class Url {
+public class UrlEntity {
 
     @Id
     private String id;
