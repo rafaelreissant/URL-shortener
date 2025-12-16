072b680834e07b00b7ad77e821ad4c5c16ab94c8
rafaelreissant <santireis14@gmail.com>
2025-12-16 18:11:34 -0300

    creating entitie url

---
+++ src/main/java/tech/buildrun/urlshortener/entities/Url.java
@@ -0,0 +1,51 @@
+package tech.buildrun.urlshortener.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "urls")
+public class Url {
+
+    @Id
+    private String id;
+
+    private String fullUrl;
+
+    @Indexed(expireAfter = "0")
+    private LocalDateTime expireAt;
+
+    public Url() {}
+
+    public Url(String id, String fullUrl, LocalDateTime expireAt) {
+        this.id = id;
+        this.fullUrl = fullUrl;
+        this.expireAt = expireAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullUrl() {
+        return fullUrl;
+    }
+
+    public void setFullUrl(String fullUrl) {
+        this.fullUrl = fullUrl;
+    }
+
+    public LocalDateTime getExpireAt() {
+        return expireAt;
+    }
+
+    public void setExpireAt(LocalDateTime expireAt) {
+        this.expireAt = expireAt;
+    }
+}
